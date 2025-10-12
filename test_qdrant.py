a739db6852d24c6d237ecd7f96f7292415eed41f
Bayu Wicaksono <bwbayuuu@gmail.com>
2025-10-12 19:27:15 +0700

    docs: add comment

--- test_qdrant.py
+++ test_qdrant.py
@@ -6,6 +6,8 @@ docker pull qdrant/qdrant
 docker run -p 6333:6333 -p 6334:6334 \
     -v "$(pwd)/qdrant_storage:/qdrant/storage:z" \
     qdrant/qdrant
+
+access dashboard : http://localhost:6333/dashboard#/collections
 """
 
 if __name__ == "__main__":
